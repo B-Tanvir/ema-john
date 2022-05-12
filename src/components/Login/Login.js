c10318aa5d09de65a2b7392ecd1f07eacf8e5ebc
b-tanvir <b_tanvir@outlook.com>
2022-05-12 13:24:22 +0600

    implement signin and signup

---
+++ src/components/Login/Login.js
@@ -0,0 +1,61 @@
+import React, {useRef, useState} from 'react';
+import './Login.css'
+import {Link, useNavigate} from "react-router-dom";
+import {useSignInWithEmailAndPassword} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+
+    const handlePasswordBlur = (e) => {
+      setPassword(e.target.value)
+    }
+
+    const handleEmailBlur = (e) => {
+      setEmail(e.target.value)
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(email, password)
+    };
+
+    return (
+        <div className={'form-container'}>
+            <div className={'input-field'}>
+                <h4 className={'login-text'}>Login</h4>
+                <form onSubmit={handleLogin}>
+                    <div className={'input-group'}>
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="" id="email" required/>
+                    </div>
+
+                    <div className={'input-group'}>
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="" id="password" required/>
+                    </div>
+                    <p>{error?.message}</p>
+                    <input className={'form-submit'} type="submit" value="Login"/>
+                </form>
+                <p>
+                    New to Ema-john? <Link className={'form-link'} to={'/signup'}>Create New Account</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
